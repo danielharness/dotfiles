9ed88e6de7309505b1937e0198d92a9e09753e20
danielharness <danielharnesswork@gmail.com>
2023-09-24 22:21:47 +0300

    added ipython default imports

---
+++ .ipython/profile_default/startup/default_imports.py
@@ -0,0 +1,18 @@
+"""
+Default imports for convenience.
+"""
+
+import json
+import os
+import re
+import struct
+import sys
+from base64 import b64decode, b64encode
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from enum import Enum
+from pathlib import Path
+from time import sleep
+from typing import *
+
+import requests
